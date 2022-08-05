f20d5d673d1e10f9959c8ec71e2273f781d1dfb5
Emma Mironova <emma2807764@yandex.ru>
2022-08-05 23:28:14 +0300

    first commit

---
+++ src/main/java/Locators.java
@@ -0,0 +1,28 @@
+public class Locators {
+//Кликабельная надпись Яндекс By.xpath("//img[@alt='Yandex']"
+//Кликабельная надпись Самокат By.xpath("//img[@alt='Scooter']"
+//Кнопка Куки  By.id("rcc-confirm-button")"
+//Кнопка Заказать By.xpath(".//button[@class='Button_Button__ra12g']"
+//Кнопка Статус Заказа By.xpath("//button[@class='Header_Link__1TAG7']"
+// Поле Номер заказа By.xpath("//input[@class='Input_Input__1iN_Z Header_Input__xIoUq']"
+// Кнопка Go By.xpath(".//button[@class='Button_Button__ra12g Header_Button__28dPO']"
+//Кнопка Заказать внизу страницы By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM']"
+//Форма для кого самока
+//Поле имя By.xpath("//input[@placeholder='* Имя']"
+//Поле Фамилия By.xpath("//input[@placeholder='* * Фамилия']"
+//Поле Адрес By.xpath("//input[@placeholder='* Адрес: куда привезти заказ']"
+// Поле станция метро =By.xpath("//input[@placeholder='* Станция метро']"
+// Всплывающий список станций метро (By.xpath("//*[@class='select-search__select']"
+// Поле телефон By.xpath("//input[@placeholder='* Телефон: на него позвонит курьер']"
+// Кнопка Далее By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM']"
+// Поле когда привезти самокат By.xpath("//input[@placeholder='* Когда привезти самокат']"
+// Поле срок аренды By.xpath("//*[@class='Dropdown-control']"
+// Всплывающий список поля срок аренды By.xpath("//*[text() = 'сутки']"
+// Поле цвет самоката By.id("black")
+// Поле Комментарий By.xpath("//input[@placeholder='Комментарий для курьера']"
+// Кнопка Заказать By.xpath("//button[@class='Button_Button__ra12g Button_Middle__1CSJM']"
+// Кнопка Да (By.xpath("//*[text() = 'Да']"
+
+
+
+}
